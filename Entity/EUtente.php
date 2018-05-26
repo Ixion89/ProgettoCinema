f604816e3cfcff349a72a515db7d9212cf351985
Teresa Bove <teresa.4us@gmail.com>
2018-05-26 17:50:57 +0200

    entityutente

--- Entity/EUtente.php
+++ Entity/EUtente.php
@@ -1,11 +1,18 @@
 <?php
-class Utente extends EGuest {
+require_once'C:\xampp\htdocs\_progetto\Entity\EProfilo.php';
+require_once'C:\xampp\htdocs\_progetto\Entity\EGuest.php';
+class EUtente extends EGuest {
       public $email;
       public $validazione;
+	  public $profilo;
       
-      public function __construct(string $em,string $val);{
-      $this->email=$em;
-      $this->validazione=$val;}
+      public function __construct(string $un,string $pw, EProfilo $Profilo)
+	  {
+      $this->set_username($un);
+      $this->set_password($pw);
+	  $this->validazione=true;
+	  $this->profilo=$Profilo;
+	  }
       public function get_email(){return $this->email;}
       public function get_validazione(){return $this->validazione;}
       public function set_email(string $valore){$this->email=$valore;}
