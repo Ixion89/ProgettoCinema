2d55e5bdad4db6ea09ccebd4eb8805f4c1080e12
teresabove <teresa.4us@gmail.com>
2018-05-27 15:26:56 +0200

    inizio classe pagamento

---
+++ Entity/EPagamento.php
@@ -0,0 +1,11 @@
+<?php
+class EPagamento{
+
+public $totale;   //float
+public $IDPagamento;  //string
+public $ListaIte;  //array di item
+
+public function __construct(){}
+
+public function costruttore(){}
+?>
